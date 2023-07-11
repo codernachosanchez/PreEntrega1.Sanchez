1851179453474b87b06ae9b7dc11b2fd25cc6004
codernachosanchez <131318682+codernachosanchez@users.noreply.github.com>
2023-07-11 17:06:40 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,57 @@
+let carrito = "";
+let nuevaOperacion = false;
+
+let producto = prompt(
+  "Ingrese el tamaño de la huerta que desea comprar (Chica, Mediana, Grande, Elebada):"
+);
+let cantidad = parseInt(
+  prompt(`Ingrese la cantidad de huertas ${producto} que desea comprar:`)
+);
+
+function hacerCarrito(producto, cantidad) {
+  do {
+    seleccionarProducto(producto, cantidad);
+    nuevaOperacion = confirm("¿Desea agregar otro producto al carrito?");
+    if (nuevaOperacion) {
+      producto = prompt(
+        "Ingrese el tamaño huerta que desea comprar (Chica, Mediana, Grande, Elebada):"
+      );
+      cantidad = parseInt(
+        prompt(`Ingrese la cantidad de huertas ${producto} que desea comprar:`)
+      );
+    }
+  } while (nuevaOperacion);
+
+  console.log("Productos en el carrito:");
+  console.log(carrito);
+}
+
+function seleccionarProducto(producto, cantidad) {
+  switch (producto.toLowerCase()) {
+    case "chica":
+      carrito += `Huertas Chicas - Cantidad: ${cantidad} - Precio Total: ${
+        15 * cantidad
+      } USD\n`;
+      break;
+    case "mediana":
+      carrito += `Huertas Medianas - Cantidad: ${cantidad} - Precio Total: ${
+        30 * cantidad
+      } USD\n`;
+      break;
+    case "grande":
+      carrito += `Huertas Grandes - Cantidad: ${cantidad} - Precio Total: ${
+        50 * cantidad
+      } USD\n`;
+      break;
+    case "elebada":
+      carrito += `Huertas Elebadas - Cantidad: ${cantidad} - Precio Total: ${
+        50 * cantidad
+      } USD\n`;
+      break;
+    default:
+      alert("El producto ingresado no está disponible.");
+      break;
+  }
+}
+
+hacerCarrito(producto, cantidad);
